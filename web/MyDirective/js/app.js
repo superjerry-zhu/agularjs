71a60820f200585aa9affa51a0b5988e526b58fd
zhu.superjerry <zhu.superjerry@gmail.com>
2016-06-29 23:41:01 +0800

    learn directive

--- web/MyDirective/js/app.js
+++ web/MyDirective/js/app.js
@@ -7,5 +7,33 @@
 			replace: true,
 			template: '<a href="http://www.baidu.com">Click me</a>'
 		};
+	}).directive("mySecondDirective", function(){
+		return {
+			restrict: 'A',
+			replace: true,
+			scope: {
+				myUrl: '@someAttr',
+				myUrlText: '@'
+			},
+			template: '<a href="{{myUrl}}">{{myUrlText}}</a>'
+		};
+	}).directive("myThirdDirective", function(){
+		return {
+			restrict: 'A',
+			scope: {
+				myUrl: '@someAttr',
+				myUrlText: '@'
+			},
+			template: '<div>\ <input type="text" ng-model="myUrl"/>\<a href="{{myUrl}}">{{myUrlText}}</a>\</div>'
+		}
+	}).directive("myFourthDirective", function(){
+		return {
+			restrict: 'A',
+			scope: {
+				myUrl: '=someAttr',
+				myUrlText: '@'
+			},
+			template: '<div>\ <input type="text" ng-model="myUrl"/>\<a href="{{myUrl}}">{{myUrlText}}</a>\</div>'
+		}
 	});
 })();
